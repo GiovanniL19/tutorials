c49ff18f9b52c573b62ae4dd21514e600f68dac2
Giovanni Lenguito <giovanni16.gl@gmail.com>
2016-11-02 14:45:30 +0000

    Implements Java Web Service

---
+++ Java/ContactWebService/src/java/contact/webservice/Contact.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contact.webservice;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Giovanni
+ */
+@Entity
+@Table(name = "CONTACT")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Contact.findAll", query = "SELECT c FROM Contact c")
+    , @NamedQuery(name = "Contact.findById", query = "SELECT c FROM Contact c WHERE c.id = :id")
+    , @NamedQuery(name = "Contact.findByCname", query = "SELECT c FROM Contact c WHERE c.cname = :cname")
+    , @NamedQuery(name = "Contact.findByCtelno", query = "SELECT c FROM Contact c WHERE c.ctelno = :ctelno")})
+public class Contact implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private BigDecimal id;
+    @Size(max = 20)
+    @Column(name = "CNAME")
+    private String cname;
+    @Size(max = 10)
+    @Column(name = "CTELNO")
+    private String ctelno;
+
+    public Contact() {
+    }
+
+    public Contact(BigDecimal id) {
+        this.id = id;
+    }
+
+    public BigDecimal getId() {
+        return id;
+    }
+
+    public void setId(BigDecimal id) {
+        this.id = id;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    public String getCtelno() {
+        return ctelno;
+    }
+
+    public void setCtelno(String ctelno) {
+        this.ctelno = ctelno;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Contact)) {
+            return false;
+        }
+        Contact other = (Contact) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "contact.webservice.Contact[ id=" + id + " ]";
+    }
+    
+}
---
+++ Java/WebServicesTest/web/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : 02-Nov-2016, 14:27:40
+    Author     : Giovanni
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
