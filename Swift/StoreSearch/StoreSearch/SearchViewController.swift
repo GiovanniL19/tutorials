35435830422c030f27ffb05531bdd9efce00a551
Giovanni Lenguito <giovanni16.gl@gmail.com>
2016-11-21 14:55:57 +0000

    Adds new swift project

---
+++ Swift/StoreSearch/StoreSearch/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Giovanni Lenguito on 21/11/2016.
+//  Copyright © 2016 Giovanni Lenguito. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
