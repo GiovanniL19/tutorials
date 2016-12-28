03d594531788671b00de52e0fbed67b84f474b39
Giovanni Lenguito <giovanni16.gl@gmail.com>
2016-12-28 22:15:53 +0000

    Updates

--- Swift/StoreSearch/StoreSearch/SearchViewController.swift
+++ Swift/StoreSearch/StoreSearch/SearchViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0,right: 0)
     }
-
-
+}
+//MARK: Extensions
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("The search text is: '\(searchBar.text!)'")
+    }
 }
 
